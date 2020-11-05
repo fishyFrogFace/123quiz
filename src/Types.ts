e18c069557fb46cbe1479fbb88037713c1cf4303
Camilla Rygh <camillahallo@mail.com>
2020-11-05 17:12:39 +0100

    Change API functions so they work better with Typescript and topics

--- src/Types.ts
+++ src/Types.ts
@@ -7,3 +7,19 @@ export interface AnswerOption {
   answerText: string;
   isCorrect: boolean;
 }
+
+export interface RawQuiz {
+  multiple_correct_answers: string;
+  question: string;
+  correct_answer: string;
+  answers: Answer;
+}
+
+export interface Answer {
+  answer_a: string;
+  answer_b: string;
+  answer_c: string;
+  answer_d: string;
+  answer_e: string;
+  answer_f: string;
+}
--- src/api/QuizAPI.tsx
+++ src/api/QuizAPI.tsx
@@ -7,44 +7,53 @@ const MAX_DELAY_MS = 0;
 // api key will be deactivated after the workshop, you will then have to get your own at https://quizapi.io
 const API_KEY = "";
 
-const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
+const delay = (ms: number) => new Promise(res => setTimeout(res, ms));
 const randomDelay = () =>
   delay(MIN_DELAY_MS + Math.random() * (MAX_DELAY_MS - MIN_DELAY_MS));
 
-export const toQuestionList = (data: any[]): Question[] =>
-  data
-    .filter((question) => question.multiple_correct_answers === "false")
-    .map((question) => ({
+export const toQuestionList = (data: RawQuiz[]): Question[] => {
+  return data
+    .filter(question => question.multiple_correct_answers === "false")
+    .map(question => ({
       questionText: question.question,
       answerOptions: [
         {
           answerText: question.answers.answer_a,
-          isCorrect: question.correct_answer === "answer_a",
+          isCorrect: question.correct_answer === "answer_a"
         },
         {
           answerText: question.answers.answer_b,
-          isCorrect: question.correct_answer === "answer_b",
+          isCorrect: question.correct_answer === "answer_b"
         },
         {
           answerText: question.answers.answer_c,
-          isCorrect: question.correct_answer === "answer_c",
+          isCorrect: question.correct_answer === "answer_c"
         },
         {
           answerText: question.answers.answer_d,
-          isCorrect: question.correct_answer === "answer_d",
+          isCorrect: question.correct_answer === "answer_d"
         },
-      ],
+        {
+          answerText: question.answers.answer_e,
+          isCorrect: question.correct_answer === "answer_e"
+        },
+        {
+          answerText: question.answers.answer_f,
+          isCorrect: question.correct_answer === "answer_f"
+        }
+      ]
     }));
+};
 
-export const fetchQuiz = async (topic: string) => {
+export const fetchQuiz = async (topic: string): Promise<RawQuiz[]> => {
   await randomDelay();
   return fetch(
-    `https://quizapi.io/api/v1/questions?apiKey=${API_KEY}&limit=10&category=${topic}&difficulty=easy`,
+    `https://quizapi.io/api/v1/questions?apiKey=${API_KEY}&limit=10&tags=${topic}&difficulty=easy`,
     {
       headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
+        "Content-Type": "application/x-www-form-urlencoded"
       },
-      method: "GET",
+      method: "GET"
     }
-  );
+  ).then(res => res.json());
 };
