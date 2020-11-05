0d8ced82ab2a8d6835315564abd8b353f97f0c15
Camilla Rygh <camillahallo@mail.com>
2020-11-05 19:51:52 +0100

    Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
-import NavBar from "./components/NavBar";
+import NavBar, { NoMatch } from "./components/NavBar";
 import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
 import Quiz from "./pages/Quiz";
 import QuizList from "./components/QuizList";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import Greeting from "./components/Greeting";
 
 interface AppState {
   selectedQuiz: string;
@@ -17,17 +19,24 @@ const App = () => {
   }, [selectedQuiz]);
 
   return (
-    <div>
+    <Router>
       <QuizList setSelectedQuiz={setSelectedQuiz} />
       <NavBar />
       <Container className="displace">
         <Grid container justify="center" style={{ marginTop: "60px" }}>
           <Grid item>
-            <Quiz />
+            <Switch>
+              <Route path="/quiz" exact component={Quiz} />
+              <Route path="/greeting" exact component={Greeting} />
+              <Route path="/" exact component={Quiz} />
+              <Route path="*">
+                <NoMatch />
+              </Route>
+            </Switch>
           </Grid>
         </Grid>
       </Container>
-    </div>
+    </Router>
   );
 };
 
